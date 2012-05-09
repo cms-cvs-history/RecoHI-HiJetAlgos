a7714e587867208d7a90202e6616deb3caa767dd
Yetkin Yilmaz <sha1-4a5323f62d82027406d0219ebeb6c985199c4826@cern.ch>
2012-05-09 13:57:55 +0000

    Large set of algos

--- python/HiRecoJets_cff.py
+++ python/HiRecoJets_cff.py
@@ -115,4 +115,43 @@ hiRecoJets = cms.Sequence(caloTowersRec*caloTowers*iterativeConePu5CaloJets)
 ## Extended Sequence
 hiRecoAllJets = cms.Sequence(caloTowersRec*caloTowers*iterativeConePu5CaloJets+iterativeConePu7CaloJets+ic5CaloJets+ic7CaloJets+akPu5CaloJets+akPu7CaloJets+ak5CaloJets+ak7CaloJets + ktPu4CaloJets + ktPu6CaloJets +  kt4CaloJets + kt6CaloJets)
 
+### Extra extended algos & sequence
+akPu6PFJets = akPu3PFJets.clone(rParam = 0.6)
+akPu6CaloJets = akPu3CaloJets.clone(rParam = 0.6)
+akPu4PFJets = akPu3PFJets.clone(rParam = 0.4)
+akPu4CaloJets = akPu3CaloJets.clone(rParam = 0.4)
+akPu2PFJets = akPu3PFJets.clone(rParam = 0.2)
+akPu2CaloJets = akPu3CaloJets.clone(rParam = 0.2)
+akPu1PFJets = akPu3PFJets.clone(rParam = 0.1)
+akPu1CaloJets = akPu3CaloJets.clone(rParam = 0.1)
+
+iterativeCone5CaloJets = iterativeConePu5CaloJets.clone(doPUOffsetCorr = False, jetPtMin = 1)
+ak6PFJets = akPu6PFJets.clone(doPUOffsetCorr = False, jetPtMin = 1)
+ak6CaloJets = akPu6CaloJets.clone(doPUOffsetCorr = False, jetPtMin = 1)
+ak5PFJets = akPu5PFJets.clone(doPUOffsetCorr = False, jetPtMin = 1)
+ak5CaloJets = akPu5CaloJets.clone(doPUOffsetCorr = False, jetPtMin = 1)
+ak4PFJets = akPu4PFJets.clone(doPUOffsetCorr = False, jetPtMin = 1)
+ak4CaloJets = akPu4CaloJets.clone(doPUOffsetCorr = False, jetPtMin = 1)
+ak3PFJets = akPu3PFJets.clone(doPUOffsetCorr = False, jetPtMin = 1)
+ak3CaloJets = akPu3CaloJets.clone(doPUOffsetCorr = False, jetPtMin = 1)
+ak2PFJets = akPu2PFJets.clone(doPUOffsetCorr = False, jetPtMin = 1)
+ak2CaloJets = akPu2CaloJets.clone(doPUOffsetCorr = False, jetPtMin = 1)
+ak1PFJets = akPu1PFJets.clone(doPUOffsetCorr = False, jetPtMin = 1)
+ak1CaloJets = akPu1CaloJets.clone(doPUOffsetCorr = False, jetPtMin = 1)
+
+recoAk1to6 = cms.Sequence( akPu1PFJets * akPu2PFJets *akPu3PFJets * akPu4PFJets * akPu5PFJets * akPu6PFJets *
+                           ak1PFJets * ak2PFJets *ak3PFJets * ak4PFJets * ak5PFJets * ak6PFJets *
+                           akPu1CaloJets * akPu2CaloJets *akPu3CaloJets * akPu4CaloJets * akPu5CaloJets * akPu6CaloJets *
+                           ak1CaloJets * ak2CaloJets *ak3CaloJets * ak4CaloJets * ak5CaloJets * ak6CaloJets
+                           )
+
+
+
+
+
+
+
+
+
+
 
